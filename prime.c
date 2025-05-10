22f0ac0226535ce7c314f041ea2bf7b105fb07fb
Thiyanesh Saminathan <thiyaneshcode@gmail.com>
2025-05-10 20:19:16 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int prime(int n)
+{
+    int i,j,flag=0;
+    for(i=1;i<=n;i++)
+    {
+        flag=0;
+        for(j=1;j<=i;j++)
+        {
+            if(i%j==0)
+            {
+                flag+=1;
+            }
+        }
+    
+        if(flag==2)
+        {
+            printf("%dis prime ",i);
+        }
+        else
+        {
+            printf(" %dnot prime ",i);
+        }
+    }
+    
+}
+int main()
+{
+    int num;
+    printf("enter the n value:");
+    scanf("%d",&num);
+    prime(num);
+    return 0;
+}
