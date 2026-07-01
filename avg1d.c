d60829b5fbdf0cdca47e2b69095f6dd9bfc4a047
Thiyanesh Saminathan <thiyaneshcode@gmail.com>
2026-07-01 12:35:55 +0530

    Add initial avg1d.c for average calculation
    Initial implementation of a program to calculate the average of numbers entered by the user. The function 'ar' is defined but not fully implemented.

---
+++ avg1d.c
@@ -0,0 +1,23 @@
+#include<Stdio.h>
+void ar(int num,int numm);
+main()
+{
+    int arr[100],c=0;
+    printf("enter the number:");
+    for(int i=0;i<arr;i++)
+    {
+    scanf("%d",&arr[i]);
+    ar(arr[i]);
+    sum=sum+arr[i];
+    }
+    ar(sum);
+    
+    //return sum;
+}
+void ar(int num,int numm);
+{
+    if(num>0)
+    {
+        z
+    }
+}
